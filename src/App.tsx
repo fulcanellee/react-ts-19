dbb2ffea74eecb762908b70e783d2995bfd073b9
Mikhail Lukin <mishalykin@mail.ru>
2026-06-15 13:08:49 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {CDNIcon} from '@alfalab/core-components/cdn-icon'
+import {CardImage} from '@alfalab/core-components/card-image'
+import {ProductCover} from '@alfalab/core-components/product-cover'
+import {Gap} from '@alfalab/core-components/gap'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        <h2>test env</h2>
+        <CDNIcon
+            color='var(--color-light-neutral-translucent-1300)'
+            name='glyph_diamonds_xxl'
+        />
+        <Gap size={16}/>
+        <CardImage
+            cardId='EG'
+        />
+        <Gap size={16}/>
+        <ProductCover.Single
+            size={164}
+            cardholderName='Cardholder Name'
+            cardNumber={1234000000001234}
+            layers='BACKGROUND,LOGO,PAYMENT_SYSTEM'
+            cardId='RM'
+            eyeButton={true}
+        />
+        <Gap size={16}/>
+    </div>
   )
 }
 
