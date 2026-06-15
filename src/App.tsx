15d3320b784a3e3d1ff86a5d32fb1f790de6b2bb
Mikhail Lukin <mishalykin@mail.ru>
2026-06-15 15:37:31 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {CDNIcon} from '@alfalab/core-components/cdn-icon'
 import {CardImage} from '@alfalab/core-components/card-image'
 import {ProductCover} from '@alfalab/core-components/product-cover'
 import {Gap} from '@alfalab/core-components/gap'
+import {BankCard} from '@alfalab/core-components/bank-card'
 
 function App() {
   return (
@@ -11,16 +12,27 @@ function App() {
             color='var(--color-light-neutral-translucent-1300)'
             name='glyph_diamonds_xxl'
         />
+        <CDNIcon
+            color='var(--color-light-neutral-700)'
+            name='glyph_diamonds_m'
+        />
+        <CDNIcon
+            color='var(--color-light-neutral-500)'
+            name='glyph_diamonds_s'
+        />
         <Gap size={16}/>
         <CardImage
             cardId='EG'
         />
+        <Gap size={16}/>
+        <BankCard />
         <Gap size={16}/>
         <ProductCover.Single
             size={164}
             cardholderName='Cardholder Name'
             cardNumber={1234000000001234}
             layers='BACKGROUND,LOGO,PAYMENT_SYSTEM'
+            baseUrl={`https://${`online.alfabank.ru`}/cards-images/cards/`}
             cardId='RM'
             eyeButton={true}
         />
